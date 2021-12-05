e117c364009e10d35117a93ee313668d9b6eba77
knitron07 <samarmahur1992@gmail.com>
2021-12-05 20:23:33 +0530

    starting navigations

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo-app-loading';
 
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+  });
+};
 export default function App() {
+  const [loadFonts, setLoadFonts] = useState(false);
+
+  if (loadFonts && loadFonts === false) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setLoadFonts(true)}
+        onError={(error) => {
+          console.log(error);
+        }}
+      />
+    );
+  }
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
